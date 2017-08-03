186370d1b3f1082f178f6f785183d2c550c73479
TannerGruebele <tanner@saambaa.com>
2017-08-03 12:44:42 -0700

    styling with bulma, trying to add large preview for slideshow

--- main.js
+++ main.js
@@ -2,12 +2,33 @@ $(document).ready(function() {
     console.log('------------------------------------');
     console.log("JS READY");
     console.log('------------------------------------');
-    $('.images').slick({
+    $('.your-class').slick({
         dots: true,
         infinite: true,
         speed: 300,
         slidesToShow: 4,
         centerMode: true,
+        variableWidth: true
+    });
+
+
+    $('.top-image').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.your-class'
+    });
+    $('.your-class').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.top-image',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
         variableWidth: true
     });
 });
